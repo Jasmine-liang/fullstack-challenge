58216600dd7046e1bc4e5d25cbff9dab5aab2154
Jasmine-liang <mingchenliaing954@gmail.com>
2022-05-25 12:58:02 +0800

    change uri to a variable

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  // uri: "http://localhost:4000"
+  uri: process.env.APOLLO_BACKEND_URL,
   cache: new InMemoryCache(),
 });
